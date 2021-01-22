1721b1bea6a98bdebf64cf16b5b355552c92072b
kuritaeiji <eiji199825@yahoo.co.jp>
2021-01-23 08:22:47 +0900

    マイページ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# require("csv")
+require("csv")
 
-# CSV.foreach('db/prefectures.csv') do |row|
-#   Prefecture.create!(name: row[1])
-# end
+CSV.foreach('db/prefectures.csv') do |row|
+  Prefecture.create!(name: row[1])
+end
 
 include(ActionDispatch::TestProcess)
 
@@ -23,5 +16,6 @@ user = User.create!(
   postal_code: '1234567',
   prefecture_id: '13',
   address: '港区',
-  image: fixture_file_upload("#{Rails.root}/spec/images/a.jpg", 'image/jpg')
+  image: fixture_file_upload("#{Rails.root}/spec/images/a.jpg", 'image/jpg'),
+  activated: true
 )

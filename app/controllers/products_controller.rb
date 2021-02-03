8d3aa5ae16b0b43ba227a9b4aed027475f8b2418
kuritaeiji <eiji199825@yahoo.co.jp>
2021-02-03 15:20:33 +0900

    商品詳細画面に取引済みでも遷移できる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action(:log_in_user, only: [:new, :create, :edit, :update, :index])
   before_action(:set_product, only: [:show, :edit, :update, :destroy])
   before_action(:correct_user, only: [:edit, :update, :destroy])
-  before_action(:untraded_product, only: [:show, :edit, :update, :destroy])
+  before_action(:untraded_product, only: [:edit, :update, :destroy])
 
   def index
   end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,8 @@ module ProductsHelper
   def get_image_index(images, image)
     images.blobs.find_index(image.blob)
   end
+
+  def trade_status(product)
+    content_tag(:span, '販売済み', class: 'badge badge-danger') if product.traded?
+  end
 end
